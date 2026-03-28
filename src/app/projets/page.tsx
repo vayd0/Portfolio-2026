1d115fb0d36b2effc434c7ce7d830ee8d7f4c1ea
vayd0 <kiiroxyz@gmail.com>
2026-03-28 12:57:16 +0100

    fix: déplacer px-16 py-20 sur le div interne

--- src/app/projets/page.tsx
+++ src/app/projets/page.tsx
@@ -22,8 +22,8 @@ export default async function ProjetsPage() {
   const projects = await sanity.fetch<Project[]>(query);
 
   return (
-    <main className="min-h-screen px-16 py-20">
-      <div className="flex flex-wrap justify-start gap-12">
+    <main className="min-h-screen">
+      <div className="flex flex-wrap justify-start gap-12 px-16 py-20">
         {projects.map((project, i) => (
           <FallIn key={project._id} delay={i * 0.05}>
             <div className="w-[380px] shrink-0">
