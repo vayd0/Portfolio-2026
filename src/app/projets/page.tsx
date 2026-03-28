2c01bd8e82f8087115bee7ac87235ce7b7ab3094
vayd0 <kiiroxyz@gmail.com>
2026-03-28 12:54:55 +0100

    feat: page /projets avec cards Sanity en flex wrap

---
+++ src/app/projets/page.tsx
@@ -0,0 +1,42 @@
+import { sanity } from "@/lib/sanity";
+import Card from "@/components/Card";
+import FallIn from "@/components/FallIn";
+
+export const revalidate = 60;
+
+type Project = {
+  _id: string;
+  title: string;
+  image?: string;
+  color?: string;
+};
+
+const query = `*[_type == "project"] | order(_createdAt asc) {
+  _id,
+  title,
+  "image": image.asset->url,
+  color
+}`;
+
+export default async function ProjetsPage() {
+  const projects = await sanity.fetch<Project[]>(query);
+
+  return (
+    <main className="min-h-screen px-16 py-20">
+      <div className="flex flex-wrap justify-start gap-12">
+        {projects.map((project, i) => (
+          <FallIn key={project._id} delay={i * 0.05}>
+            <div className="w-[380px] shrink-0">
+              <Card
+                title={project.title}
+                image={project.image}
+                color={project.color}
+                index={i}
+              />
+            </div>
+          </FallIn>
+        ))}
+      </div>
+    </main>
+  );
+}
